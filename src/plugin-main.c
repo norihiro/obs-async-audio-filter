7b8c08285920ba1d1e336ccbf4ae0efb4bd8d92e
Norihiro Kamae <norihiro@nagater.net>
2022-11-02 20:43:56 +0900

    src/plugin-main.c: Remove a word "successfully"
    APIs obs_register_* do not return failure status.

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -29,7 +29,7 @@ extern const struct obs_source_info asynchronous_audio_info;
 bool obs_module_load(void)
 {
 	obs_register_source(&asynchronous_audio_info);
-	blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	blog(LOG_INFO, "plugin loaded (version %s)", PLUGIN_VERSION);
 	return true;
 }
 
