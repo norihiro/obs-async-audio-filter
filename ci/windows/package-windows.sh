3790b8acec8cc39c1de5070f3430dd2dbb1e4d48
Norihiro Kamae <norihiro@nagater.net>
2024-01-10 20:23:24 +0900

    ci: Use bash script instead of batch file

---
+++ ci/windows/package-windows.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -ex
+. ci/ci_includes.generated.sh
+
+PackageVersion="$(git describe --tags --always)"
+
+mkdir package
+cd package
+
+cp ../LICENSE ../release/data/obs-plugins/${PLUGIN_NAME}/LICENCE-${PLUGIN_NAME}.txt
+cp ../deps/libsamplerate/COPYING ../release/data/obs-plugins/${PLUGIN_NAME}/LICENCE-libsamplerate.txt
+
+7z a "${PLUGIN_NAME}-${PackageVersion}-obs$1-Windows.zip" ../release/*
+cmd.exe <<EOF
+iscc ..\\installer\\installer-Windows.generated.iss /O. /F"${PLUGIN_NAME}-${PackageVersion}-obs$1-Windows-Installer"
+EOF
+sleep 2 && echo
+
+sha1sum \
+	"${PLUGIN_NAME}-${PackageVersion}-obs$1-Windows.zip" \
+	"${PLUGIN_NAME}-${PackageVersion}-obs$1-Windows-Installer.exe"
